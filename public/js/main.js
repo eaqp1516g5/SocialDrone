949ed03227cdf71174dcc71837058da333f15abe
JavierCancer <javi_vi489@hotmail.com>
2016-03-16 23:06:47 +0100

    post con angular

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+angular.module("ListarSocialDrone",[])
+    .controller("FirstController",function($scope,$http){
+        $scope.users = {};
+        $scope.newPost = {};
+        $scope.deluser = {};
+        $http.get("http://localhost:3000/users") //hacemos get de todos los users
+            .success(function(data){
+                console.log(data);
+                $scope.users= data;
+            })
+            .error(function(err){
+
+            });
+        $scope.addUser = function() {
+            $http.post("http://localhost:3000/users", {
+                    username: $scope.newPost.username,
+                    password: $scope.newPost.password,
+                    email: $scope.newPost.email,
+                    gender: $scope.newPost.gender,
+                    address: $scope.newPost.address
+
+
+                })
+                .success(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.posts.push($scope.newPost);
+                    $scope.addPost = {}; //limpiamos
+                })
+                .error(function (error, status, headers, config) {
+                    console.log(error);
+                });
+        }
+        $scope.deleteUser = function (id) {
+            $http.delete("http://localhost:3000/user/" + id, {})
+                .success(function (data, status, headers, config) {
+                    console.log("Borraaado");
+                })
+                .error(function (error, status, headers, config) {
+                    console.log(error);
+                });
+        }
+
+    });
