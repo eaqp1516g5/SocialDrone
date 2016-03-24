4e8649cb33a8ef67eb6a1f57b39571cc951e3b97
marcb10 <marc.ballesteros@hotmail.com>
2016-03-24 20:12:31 +0100

    angular

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,6 @@ angular.module("ListarSocialDrone",[])
         $scope.users = {};
         $scope.newPost = {};
         $scope.deluser = {};
-        $scope.MessagePost = {};
-
         $http.get("http://localhost:3000/users") //hacemos get de todos los users
             .success(function(data){
                 console.log(data);
@@ -16,10 +14,12 @@ angular.module("ListarSocialDrone",[])
         $scope.addUser = function() {
             $http.post("http://localhost:3000/users", {
                     username: $scope.newPost.username,
+                    name: $scope.newPost.name,
                     password: $scope.newPost.password,
-                    email: $scope.newPost.email,
+                    mail: $scope.newPost.mail,
                     gender: $scope.newPost.gender,
-                    address: $scope.newPost.address
+                    address: $scope.newPost.address,
+                    lastname: $scope.newPost.lastname
 
 
                 })
@@ -41,34 +41,13 @@ angular.module("ListarSocialDrone",[])
                 });
         }
         $scope.deleteUser = function (id) {
-            $http.delete("http://localhost:3000/user/" + id, {})
+            $http.delete("http://localhost:3000/users/" + id, {})
                 .success(function (data, status, headers, config) {
                     console.log("Borraaado");
                     $http.get("http://localhost:3000/users") //hacemos get de todos los users
                         .success(function(data){
                             console.log(data);
                             $scope.users= data;
-                        })
-                        .error(function(err){
-
-                        });
-                })
-                .error(function (error, status, headers, config) {
-                    console.log(error);
-                });
-        }
-        $scope.addMessage = function() {
-            $http.post("http://localhost:3000/message", {
-                    username: marc,
-                    text: $scope.MessagePost.text
-                })
-                .success(function (data, status, headers, config) {
-                    console.log(data);
-                    //$scope.posts.push($scope.newPost);
-                    // $scope.addPost = {}; //limpiamos
-                    $http.get("http://localhost:3000/message") //hacemos get de todos los users
-                        .success(function(data){
-                            console.log(data);
                         })
                         .error(function(err){
 
