f0a42c41c3adf2d97d5045709a8a590b966ea006
marcb10 <marc.ballesteros@hotmail.com>
2016-04-14 21:38:30 +0200

    arreglos y update

--- public/controllers/HomeCtrl.js
+++ public/controllers/HomeCtrl.js
@@ -4,20 +4,22 @@
 angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
     $scope.messages = {};
     $scope.message1 = {};
+    $scope.editMessage = {};
     $scope.newMessage = {};
     $scope.newComment = {};
     $scope.comment = {};
     var base_url_produccio = "http://147.83.7.159:8080";
     var base_url = "http://localhost:8080";
-    $http.get(base_url+"/message") //hacemos get de todos los users
-        .success(function(data){
-            $scope.messages= data;
-        })
-        .error(function(err){
-            var myAlert = $alert({
-                title: 'Error!', content: error, container:'#alerts-container',
-                placement: 'top', duration:3, type: 'danger', show: true});
-        });
+    getMessage();
+    function getMessage() {
+        $http.get(base_url + "/message") //hacemos get de todos los messages
+            .success(function (data) {
+                $scope.messages = data;
+            })
+            .error(function (err) {
+                console.log(err);
+            });
+    }
     $scope.enviarMensaje = function(id) {
         if(id==undefined) {
             $http.post(base_url + "/message", {
@@ -25,16 +27,7 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                     text: $scope.newMessage.message
                 })
                 .success(function (data, status, headers, config) {
-                    $http.get(base_url + "/message") //hacemos get de todos los users
-                        .success(function (data) {
-                            $scope.messages = data;
-                        })
-                        .error(function (err) {
-                            var myAlert = $alert({
-                                title: 'Error!', content: error, container: '#alerts-container',
-                                placement: 'top', duration: 3, type: 'danger', show: true
-                            });
-                        });
+                    getMessage();
                 })
                 .error(function (error, status, headers, config) {
                     console.log(error);
@@ -49,12 +42,10 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                         .success(function (data) {
                             $scope.message1 = data;
                             $scope.comment = data.comment;
+                            $scope.newComment.text=null;
                         })
                         .error(function (err) {
-                            var myAlert = $alert({
-                                title: 'Error!', content: error, container: '#alerts-container',
-                                placement: 'top', duration: 3, type: 'danger', show: true
-                            });
+                            console.log(err);
                         });
                 })
                 .error(function (error, status, headers, config) {
@@ -71,53 +62,47 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                         $scope.comment = data.comment;
                     })
                     .error(function(err){
-                        var myAlert = $alert({
-                            title: 'Error!', content: error, container:'#alerts-container',
-                            placement: 'top', duration:3, type: 'danger', show: true});
+                       console.log(err);
                     });
             })
             .error(function (error, status, headers, config) {
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+                console.log(err);
             });
     }
     $scope.borrarMensaje = function (id) {
         $http.delete(base_url+"/message/" + id, {})
             .success(function (data, status, headers, config) {
-                $http.get(base_url+"/message") //hacemos get de todos los users
-                    .success(function(data){
-                        $scope.messages= data;
-                    })
-                    .error(function(err){
-                        var myAlert = $alert({
-                            title: 'Error!', content: error, container:'#alerts-container',
-                            placement: 'top', duration:3, type: 'danger', show: true});
-                    });
+                getMessage();
             })
             .error(function (error, status, headers, config) {
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+               console.log(err);
             });
     }
+    $scope.updateMessage = function (id) {
+        $http.put(base_url+'/message/'+id,{
+            text: $scope.editMessage.text
+        }).success(function () {
+                $http.get(base_url+"/message/"+id) //hacemos get de todos los users
+                    .success(function(data){
+                        $scope.message1= data;
+                        $scope.comment = data.comment;
+                    })
+                    .error(function(err){
+                      console.log(err);
+                    });
+                $scope.editMessage.text=null;
+            })
+            .error(function (error, status, headers, config) {
+                console.log(error);
+            });
+    };
     $scope.LikeMensaje = function (id) {
         $http.post(base_url+"/message/" + id +"/like" , {})
             .success(function (data, status, headers, config) {
-                $http.get(base_url+"/message") //hacemos get de todos los users
-                    .success(function(data){
-                        $scope.messages= data;
-                    })
-                    .error(function(err){
-                        var myAlert = $alert({
-                            title: 'Error!', content: error, container:'#alerts-container',
-                            placement: 'top', duration:3, type: 'danger', show: true});
-                    });
+                getMessage();
             })
             .error(function (error, status, headers, config) {
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+                console.log(error);
             });
     }
     $scope.LikeComment = function (id, idc) {
@@ -129,15 +114,11 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                         $scope.message1 = data;
                     })
                     .error(function(err){
-                        var myAlert = $alert({
-                            title: 'Error!', content: error, container:'#alerts-container',
-                            placement: 'top', duration:3, type: 'danger', show: true});
+                        console.log(err);
                     });
             })
             .error(function (error, status, headers, config) {
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+               console.log(error);
             });
     }
     $scope.verMensaje = function(id) {
@@ -147,9 +128,7 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                 $scope.message1 = data;
             })
             .error(function(err){
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+                console.log(err);
             });
     };
     $scope.LikeMensajeint = function (id) {
@@ -161,15 +140,11 @@ angular.module('SocialDrone').controller('HomeCtrl', function ($scope, $http) {
                         $scope.comment = data.comment;
                     })
                     .error(function(err){
-                        var myAlert = $alert({
-                            title: 'Error!', content: error, container:'#alerts-container',
-                            placement: 'top', duration:3, type: 'danger', show: true});
+                        console.log(err);
                     });
             })
             .error(function (error, status, headers, config) {
-                var myAlert = $alert({
-                    title: 'Error!', content: error, container:'#alerts-container',
-                    placement: 'top', duration:3, type: 'danger', show: true});
+                console.log(error);
             });
     }
 });
