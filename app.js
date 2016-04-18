037f54ab733561613d1b034098db129f872c876c
Bernat Mir <bernatmir13@gmail.com>
2016-04-18 11:25:25 +0200

    CORS added

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ app.use(expressSession({secret: 'mySecretKey'}));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, Authorization");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 
